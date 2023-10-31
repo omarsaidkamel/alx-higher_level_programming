f433358e8a4ae08b3a8f8041cd21f3f7d699bed4
root <osk09876@gmail.com>
2023-10-31 12:40:11 -0700

    done

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-str1 = " and is greater than 5"
-str2 = " and is 0"
-str3 = " and is less than 6 and not 0"
 if number < 0:
     l = number % -10
 else:
