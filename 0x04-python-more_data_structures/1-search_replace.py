a5bafc93889fffdb125a9b8b91e47724f2a6e38a
root <osk09876@gmail.com>
2023-11-08 12:22:38 -0800

    done

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    """
+    Replaces all occurrences of an element by another in a new list
+    ...
+
+    Parameters
+    ----------
+    my_list : list
+        Initial list of elements
+    search : integer
+        The element to replace in the list
+    replace : integer
+        The new element
+
+    Return:
+        a new list with the new elements
+    """
+
     L = my_list.copy()
     x = 0
     for i in L:
