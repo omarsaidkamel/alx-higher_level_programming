d62e933792bc85991ad60ba597f6af2a269705fd
root <osk09876@gmail.com>
2023-12-04 12:32:11 -0800

    done

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+=============================
+Module with the method lookpu
+=============================
+"""
+
+
 def lookup(obj):
     """Function for return the attributes for an object"""
 
