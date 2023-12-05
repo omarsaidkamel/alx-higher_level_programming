97116195c58f9707d8751e78de2b50a18e6a60f9
root <osk09876@gmail.com>
2023-12-05 08:25:54 -0800

    done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    with open(finlename, "r", encoding="utf-8") as file:
-        for line in file:
-            print(line, end="")
+    with open(finlename, "r", encoding="UTF-8") as file:
+        print(file.read(), end="")
