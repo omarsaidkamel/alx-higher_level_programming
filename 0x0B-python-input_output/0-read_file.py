67e41465d5c4efbc01afce73e43713532f2b2a10
root <osk09876@gmail.com>
2023-12-05 08:27:04 -0800

    done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+"""
+Write a function that reads a text file (UTF8) and prints it to stdout
+"""
+
 def read_file(filename=""):
     with open(finlename, "r", encoding="UTF-8") as file:
         print(file.read(), end="")
