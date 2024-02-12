723ca003ccb9f0b20aaa678b61a7801448c5a106
omarsaidkamel <osk09876@gmail.com>
2024-02-12 14:09:21 -0800

    done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // script that searches the second biggest integer in the list of arguments
-const omar = process.argv;
-if (omar.length <= 3) {
+const mySol = process.argv;
+if (mySol.length <= 3) {
   console.log(0);
 } else {
-  console.log(omar.sort((x, y) => y - x).slice(3)[0]);
-}i
+  console.log(mySol.sort((x, y) => y - x).slice(3)[0]);
+}
